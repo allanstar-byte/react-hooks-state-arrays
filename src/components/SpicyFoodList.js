7dfe6c4ce6baa99b664a5b1da680308b1259b06e
allanstar-byte <allanmutisya263@gmail.com>
2022-07-18 15:27:29 +0300

    Arrays

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -2,25 +2,26 @@ import React, { useState } from "react";
 import { spicyFoods, getNewRandomSpicyFood } from "../data";
 
 function SpicyFoodList() {
-  const [foods, setFoods] = useState(spicyFoods);
+    const [foods, setFoods] = useState(spicyFoods);
 
-  function handleAddFood() {
-    const newFood = getNewRandomSpicyFood();
-    console.log(newFood);
-  }
+    function handleAddFood() {
+        const newFood = getNewRandomSpicyFood();
+        const newFoodArray = [...foods, newFood];
+        setFoods(newFoodArray);
+    }
 
-  const foodList = foods.map((food) => (
-    <li key={food.id}>
-      {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
-    </li>
-  ));
+    const foodList = foods.map((food) => ( <
+        li key = { food.id } > { food.name } | Heat: { food.heatLevel } | Cuisine: { food.cuisine } <
+        /li>
+    ));
 
-  return (
-    <div>
-      <button onClick={handleAddFood}>Add New Food</button>
-      <ul>{foodList}</ul>
-    </div>
-  );
+    return ( <
+        div >
+        <
+        button onClick = { handleAddFood } > Add New Food < /button> <
+        ul > { foodList } < /ul> < /
+        div >
+    );
 }
 
 export default SpicyFoodList;
